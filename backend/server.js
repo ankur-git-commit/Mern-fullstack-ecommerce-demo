03705c80b175f86413a30022463a5979adc64472
ankur-git-commit <ankur.kumar.dev@outlook.com>
2025-04-28 02:32:52 +0530

    error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import connectDB from "./config/db.js";
 import colors from 'colors'
 
 // Middleware function
-
+import { errorHandler, notFound } from "./middleware/errorMiddlware.js";
 
 // Route imports
 import productRoutes from './routes/productRoutes.js'
@@ -18,12 +18,16 @@ const app = express();
 // Define port
 const PORT = process.env.PORT || 3000;
 
-
-
+// home route
 app.get("/", (request, response) => {
   response.send("API is running....");
 });
 
+// routes
 app.use("/api/products", productRoutes)
+
+// use middleware
+app.use(errorHandler)
+app.use(notFound)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`.bgYellow));
